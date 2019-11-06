49f1a8b717f43ad296f54ce1ed3d1d3b40faee15
Marshall Bowers <elliott.codes@gmail.com>
2019-11-06 08:06:44 -0500

    Begin setting up unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Test.Hspec
+import Casing as Casing
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Casing.toCamelCase" $ do
+        it "properly converts from camelCase" $ do
+            Casing.toCamelCase "camelCase" `shouldBe` "camelCase"
+
+        it "properly converts from PascalCase" $ do
+            Casing.toCamelCase "PascalCase" `shouldBe` "pascalCase"
+
+        it "properly converts from snake_case" $ do
+            Casing.toCamelCase "snake_case" `shouldBe` "snakeCase"
+
+        it "properly converts from SCREAMING_SNAKE_CASE" $ do
+            Casing.toCamelCase "SCREAMING_SNAKE_CASE" `shouldBe` "screamingSnakeCase"
+
+        xit "properly converts from kebab-case" $ do
+            Casing.toCamelCase "kebab-case" `shouldBe` "kebabCase"
+
+        xit "properly converts from Title Case" $ do
+            Casing.toCamelCase "Title Case" `shouldBe` "titleCase"
