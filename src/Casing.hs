455feeff0c64cb21b6876b927f5551d2ccf3dfaf
Marshall Bowers <elliott.codes@gmail.com>
2019-11-08 21:57:24 -0500

    Reorganize code

--- src/Casing.hs
+++ src/Casing.hs
@@ -23,6 +23,21 @@ import qualified Data.Char as Char
 import Data.List (intercalate)
 import Data.Maybe (catMaybes)
 
+stringToMaybe :: String -> Maybe String
+stringToMaybe [] = Nothing
+stringToMaybe value = Just value
+
+mapHead :: (a -> a) -> [a] -> [a]
+mapHead _mapping [] = []
+mapHead mapping (x:xs) = mapping x : xs
+
+mapTail :: (a -> a) -> [a] -> [a]
+mapTail _mapping [] = []
+mapTail mapping (x:xs) = x : map mapping xs
+
+capitalize :: String -> String
+capitalize = mapHead Char.toUpper . mapTail Char.toLower
+
 isSeparator :: Char -> Bool
 isSeparator '_' = True
 isSeparator '-' = True
@@ -35,44 +50,29 @@ isBoundary _currentChar nextChar
 isBoundary currentChar nextChar =
   Char.isLower currentChar && Char.isUpper nextChar
 
-getWords' :: String -> [String] -> [Char] -> [String]
-getWords' currentWord acc [] = currentWord : acc
-getWords' currentWord acc (singleChar:[]) = (currentWord ++ [singleChar]) : acc
-getWords' currentWord acc (currentChar:nextChar:remainingChars) =
-  let appendCurrentChar word =
-        if isSeparator currentChar
-          then word
-          else word ++ [currentChar]
-      (currentWord', acc') =
-        if isBoundary currentChar nextChar
-          then ("", appendCurrentChar currentWord : acc)
-          else if all Char.isUpper currentWord &&
-                  Char.isUpper currentChar && Char.isLower nextChar
-                 then (appendCurrentChar "", currentWord : acc)
-                 else (appendCurrentChar currentWord, acc)
-      remainingChars' =
-        if not $ isSeparator nextChar
-          then nextChar : remainingChars
-          else remainingChars
-   in getWords' currentWord' acc' remainingChars'
-
-stringToMaybe :: String -> Maybe String
-stringToMaybe [] = Nothing
-stringToMaybe value = Just value
-
 getWords :: String -> [String]
 getWords value = reverse $ catMaybes $ map stringToMaybe $ getWords' "" [] value
-
-mapHead :: (a -> a) -> [a] -> [a]
-mapHead _mapping [] = []
-mapHead mapping (x:xs) = mapping x : xs
-
-mapTail :: (a -> a) -> [a] -> [a]
-mapTail _mapping [] = []
-mapTail mapping (x:xs) = x : map mapping xs
-
-capitalize :: String -> String
-capitalize = mapHead Char.toUpper . mapTail Char.toLower
+  where
+    getWords' currentWord acc [] = currentWord : acc
+    getWords' currentWord acc (singleChar:[]) =
+      (currentWord ++ [singleChar]) : acc
+    getWords' currentWord acc (currentChar:nextChar:remainingChars) =
+      let appendCurrentChar word =
+            if isSeparator currentChar
+              then word
+              else word ++ [currentChar]
+          (currentWord', acc') =
+            if isBoundary currentChar nextChar
+              then ("", appendCurrentChar currentWord : acc)
+              else if all Char.isUpper currentWord &&
+                      Char.isUpper currentChar && Char.isLower nextChar
+                     then (appendCurrentChar "", currentWord : acc)
+                     else (appendCurrentChar currentWord, acc)
+          remainingChars' =
+            if not $ isSeparator nextChar
+              then nextChar : remainingChars
+              else remainingChars
+       in getWords' currentWord' acc' remainingChars'
 
 -- | Converts the given string to camelCase.
 --
